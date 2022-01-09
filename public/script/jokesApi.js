54826c8d027753ea54bde15e30bb9f031796cea7
rishikagupta2468 <rishikagupta246813579@gmail.com>
2022-01-09 16:57:46 +0530

    add source code

--- public/script/jokesApi.js
+++ public/script/jokesApi.js
@@ -2,19 +2,18 @@ const jokeBtn = document.getElementById('jokeButton');
 const voice = document.getElementById('voice');
 const jokeText = document.getElementById('jokeText');
 const speech = new SpeechSynthesisUtterance();
-var synth = window.speechSynthesis;
 
 
 //bind bind with the function when window is successfully loaded
 window.onload = function () {
     jokeBtn.addEventListener('click',jokes);
-    synth.addEventListener('voiceschanged',updateVoice);
+    speechSynthesis.addEventListener('voiceschanged',updateVoice);
 };
 
 
 //Jokes fetch api
 function jokes(){
-    synth.cancel()
+    speechSynthesis.cancel()
     fetch('https://v2.jokeapi.dev/joke/Programming?type=single')
     .then(res => res.json())
     .then(data =>{
@@ -26,20 +25,20 @@ function jokes(){
 function tellJoke(){
     speech.text = jokeText.innerText;
     if(speech.voice)
-        synth.speak(speech);
+        window.speechSynthesis.speak(speech);
 }
 
 function updateVoice(){
-    const voices = synth.getVoices()
-    voices.map((v,i)=>{
-        const option = document.createElement('option');
-        option.value = i;
-        option.textContent = v.name;
+    const voices = speechSynthesis.getVoices()
+    for(var i = 0; i < 5 ; i++) {
+        var option = document.createElement('option');
+        option.textContent = voices[i].name;
+        option.value = i
         voice.appendChild(option);
-    })
+      }
 
     voice.addEventListener('change',(e)=>{
-        synth.cancel()
+        speechSynthesis.cancel()
         const selectedVoice = e.target.value;
         speech.voice = voices[selectedVoice]
         tellJoke()
